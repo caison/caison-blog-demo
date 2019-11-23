786d0ecafef879b024b521f2a8049d92ca10af91
caison <caison@aliyun.com>
2019-11-23 14:46:41 +0800

    <update> add netty demo

---
+++ io-demo/src/main/java/org/caison/io/demo/netty/NettyServerDemo.java
@@ -0,0 +1,50 @@
+package org.caison.io.demo.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.util.Date;
+
+/**
+ * @author ChenCaihua
+ * @date 2019年11月20日
+ */
+public class NettyServerDemo {
+    public static void main(String[] args) {
+        // 创建mainReactor
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        // 创建subReactor
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        final ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                // 组装NioEventLoopGroup
+                .group(bossGroup, workerGroup)
+                // 设置channel类型为NIO类型
+                .channel(NioServerSocketChannel.class)
+                // 设置网络连接配置参数
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                // 配置入站、出站事件handler
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        // 配置入站、出站事件channel
+                        //ch.pipeline().addLast(...);
+                        //ch.pipeline().addLast(...);
+                    }
+                });
+        // 绑定端口
+        int port = 8080;
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println(new Date() + ": 端口[" + port + "]绑定成功!");
+                return;
+            }
+            System.err.println("端口[" + port + "]绑定失败!");
+        });
+    }
+}
